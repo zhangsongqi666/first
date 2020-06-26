0226fa76969c04a1e1f9ff3638b596c7491b0cdb
tom_glb <zhang_glb@atguigu.com>
2020-06-26 10:31:29 +0800

    把本地工程添加到git

--- git-test-first/src/com/bjpowernode/FirstClass.java
+++ git-test-first/src/com/bjpowernode/FirstClass.java
@@ -7,4 +7,8 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
     }
+    public void test(){
+        int i = 0;
+        i+=10;
+    }
 }
