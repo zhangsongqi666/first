283ed01a0615ff5302a2f38c6cd981acac1825d2
tom_glb <zhang_glb@atguigu.com>
2020-06-26 10:39:31 +0800

    修改

--- git-test-first/src/com/bjpowernode/FirstClass.java
+++ git-test-first/src/com/bjpowernode/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+        //test
     }
     public void test(){
         int i = 0;
